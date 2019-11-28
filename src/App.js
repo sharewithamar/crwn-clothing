ee619139137e42a62753b50d2cfd06f25052122e
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-11-28 12:19:21 +0530

    sign in component setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shoppage.component';
 import Header from "./components/header/header.component"
- 
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'; 
 
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
+
       </Switch>
     </div>
   );
