ed1b68a4a3861b6d8db6e5d6ba574d4fb7bace5c
Amarnath <sharewithamar@gmail.com>
2019-12-07 23:13:59 -0800

    card-icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {ReactComponent as Logo} from '../../assets/crown.svg';
-import {auth} from '../../firebase/firebase.utils';
-import './header.styles.scss'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
+import './header.styles.scss';
 
-
-const Header = ({currentUser}) => (
-    <div className="header">
-     <Link to='/' className='logo-container'>
-     <Logo className='logo'/>
-     </Link>
-     <div className='options'>
-      <Link className='option' to='/shop'> SHOP </Link>
-      <Link className='option' to='/contact'> CONTACT </Link>
-      {
-        currentUser ? <div className='option' onClick={()=> auth.signOut()}> SIGN OUT</div> : <Link className='option' to='/signin'> SIGN IN </Link> 
-      }
-     
-     </div>
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link to='/' className='logo-container'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        {' '}
+        SHOP{' '}
+      </Link>
+      <Link className='option' to='/contact'>
+        {' '}
+        CONTACT{' '}
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          {' '}
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          {' '}
+          SIGN IN{' '}
+        </Link>
+      )}
+      <CartIcon />
     </div>
+  </div>
 );
 
 const mapStateToProps = state => ({
-  currentUser : state.user.currentUser
-})
+  currentUser: state.user.currentUser
+});
 
 export default connect(mapStateToProps)(Header);
