6eba3060a18b451ae2ace3ab785def2304cc7e55
Amarnath <sharewithamar@gmail.com>
2019-12-08 06:37:40 -0800

    card item reducer

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -4,16 +4,17 @@ import './collection-preview.styles.scss';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({title, items}) => (
- <div className="collection-preview">
- <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
-     {items.filter( (item,index) => index <4).map( ({id,...otherItemProps}) => (
-         <CollectionItem key={id} {...otherItemProps} />
-     ))}
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, index) => index < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
- </div>
-
+  </div>
 );
 
 export default CollectionPreview;
