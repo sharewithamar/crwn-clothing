fa2e4e85d2256216a566f7df7fbfab8e64905a5a
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-12-09 12:22:49 +0530

    cart-item component and connected with dropdown

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,7 +5,7 @@ import './collection-item.styles.scss';
 import { addItem } from '../../redux/cart/cart.actions';
 
 const CollectionItem = ({ item, addItem }) => {
-  const { id, name, price, imageUrl } = item;
+  const { name, price, imageUrl } = item;
 
   return (
     <div className='collection-item'>
