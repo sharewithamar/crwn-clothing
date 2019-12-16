81998dfb65023ea8ac859cf429b7296e70a6b419
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-12-16 16:19:23 +0530

    bringing collection data from firestore to app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -50,6 +50,21 @@ export const addCollectionAndDocuments = async (
   return await batch.commit(); //if success will return void
 };
 
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  //console.log(transformedCollection);
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
