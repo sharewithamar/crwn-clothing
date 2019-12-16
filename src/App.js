bdde610dddd66444b8f23eacd8514c32592ffe3f
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-12-16 14:18:48 +0530

    programatically adding batch data for collection - To be removed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shoppage.component';
-import CheckoutPage from './pages/checkout/checkout.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
-import {createStructuredSelector} from 'reselect';
-import {selectCurrentUser} from './redux/user/user.selector';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shoppage.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments
+} from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "./redux/user/user.selector";
+import { selectCollectionsForPreview } from "./redux/shop/shop.selector";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -28,6 +33,10 @@ class App extends React.Component {
         });
       } else {
         setCurrentUser(userAuth);
+        addCollectionAndDocuments(
+          "collections",
+          collectionsArray.map(({ title, items }) => ({ title, items }))
+        );
       }
     });
   }
@@ -40,15 +49,15 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/checkout' component={CheckoutPage}/>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/checkout" component={CheckoutPage} />
           <Route
             exact
-            path='/signin'
+            path="/signin"
             render={() =>
               this.props.currentUser ? (
-                <Redirect to='/' />
+                <Redirect to="/" />
               ) : (
                 <SignInAndSignUpPage />
               )
@@ -59,8 +68,9 @@ class App extends React.Component {
     );
   }
 }
-const mapStateToProps =createStructuredSelector ({
-  currentUser: selectCurrentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,41 +1,54 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCpKF07a9LM70NWKmLjj54saMSXB_G8V0A',
-  authDomain: 'crwn-db-ee351.firebaseapp.com',
-  databaseURL: 'https://crwn-db-ee351.firebaseio.com',
-  projectId: 'crwn-db-ee351',
-  storageBucket: 'crwn-db-ee351.appspot.com',
-  messagingSenderId: '643272912629',
-  appId: '1:643272912629:web:7585b1ee14fc7f00429fbc',
-  measurementId: 'G-2PNCPE177E'
+  apiKey: "AIzaSyCpKF07a9LM70NWKmLjj54saMSXB_G8V0A",
+  authDomain: "crwn-db-ee351.firebaseapp.com",
+  databaseURL: "https://crwn-db-ee351.firebaseio.com",
+  projectId: "crwn-db-ee351",
+  storageBucket: "crwn-db-ee351.appspot.com",
+  messagingSenderId: "643272912629",
+  appId: "1:643272912629:web:7585b1ee14fc7f00429fbc",
+  measurementId: "G-2PNCPE177E"
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) return;
+  if (!userAuth) return;
 
-   const userRef = firestore.doc(`users/${userAuth.uid}`);
-   const snapshot = await userRef.get();
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
 
-   //console.log(snapshot);
-   if(!snapshot.exists)
-   {
-      const {displayName ,email} = userAuth;
-      const createdAt = new Date();
-      try{
-        await userRef.set({displayName , email , createdAt, ...additionalData})
-
-      } catch(err){
-        console.log('error creating user',err.message);
-      }
-   }
+  //console.log(snapshot);
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (err) {
+      console.log("error creating user", err.message);
+    }
+  }
   return userRef;
- // console.log(firestore.doc('users/1234fdfa')); - does not get from db always returns details
-}
+  // console.log(firestore.doc('users/1234fdfa')); - does not get from db always returns details
+};
 
 firebase.initializeApp(config);
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc(); // gets a document reference with unique random id
+    // console.log(newDocRef);
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit(); //if success will return void
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
@@ -43,7 +56,7 @@ export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 
 provider.setCustomParameters({
-  prompt: 'select_account'
+  prompt: "select_account"
 });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
