f663c70d045b3653a3c309a06aef54db7498d47d
Amarnath <sharewithamar@gmail.com>
2019-12-17 16:07:12 -0800

    promise and fetch alternatives to obeserver

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import { connect } from "react-redux";
-import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import CollectionPage from "../collection/collection.component";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 import {
   firestore,
   convertCollectionsSnapshotToMap
-} from "../../firebase/firebase.utils";
+} from '../../firebase/firebase.utils';
 
-import WithSpinner from "../../components/with-spinner/with-spinner.component";
-import { updateCollections } from "../../redux/shop/shop.actions";
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
@@ -23,8 +23,23 @@ class ShopPage extends React.Component {
 
   componentDidMount() {
     const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
+    const collectionRef = firestore.collection('collections');
 
+    //using native fetch API
+    /*  fetch(`https://firestore.googleapis.com/v1/projects/crwn-db-ee351/databases/(default)/documents/collections
+    `)
+      .then(response => response.json())
+      .then(collections => console.log(collections)); */
+
+    //using promise style rest call
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+
+    //using observor style
+    /*
     collectionRef.onSnapshot(async snapshot => {
       //console.log(snapshot);
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
@@ -32,13 +47,14 @@ class ShopPage extends React.Component {
       this.setState({ loading: false });
       // console.log(collectionsMap);
     });
+    */
   }
 
   render() {
     const { match } = this.props;
     const { loading } = this.state;
     return (
-      <div className="shop-page">
+      <div className='shop-page'>
         <Route
           exact
           path={`${match.path}`}
