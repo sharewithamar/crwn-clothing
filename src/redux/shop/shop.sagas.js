9105a210eee2d27a41646e2567b89ced68d3c700
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-12-20 14:34:46 +0530

    basic saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,28 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from "./shop.actions";
 
 import ShopActionTypes from "./shop.types";
 
 function* fetchCollectionsAsync() {
   yield console.log("iam fired");
+  const collectionRef = firestore.collection("collections");
+  try {
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    put(fetchCollectionsFailure(error.errorMessage));
+  }
 }
 
 export function* fetchCollectionsStart() {
