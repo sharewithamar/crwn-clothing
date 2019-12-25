518bb2de38698be20bea1e69706cf2e6b0edfeca
Amarnath <sharewithamar@gmail.com>
2019-12-24 23:32:02 -0800

    clear cart Saga

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,9 +1,9 @@
-import { CartActionTypes } from "./cart.types";
+import { CartActionTypes } from './cart.types';
 import {
   addItemToCart,
   clearItemFromCart,
   removeItemFromCart
-} from "./cart.utils";
+} from './cart.utils';
 const INITIAL_STATE = {
   hidden: true,
   cartItems: []
@@ -30,6 +30,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems: []
       };
     default:
       return state;
--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest, all } from 'redux-saga/effects';
 import {
   firestore,
   convertCollectionsSnapshotToMap
@@ -30,4 +30,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
