572214a6f26f1a04c300b043320f13b1e7e9cc25
Amarnath <sharewithamar@gmail.com>
2020-01-09 07:09:35 +0530

    Mobile Responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import './App.css';
+import { GlobalStyle } from './global.styles';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
 import CheckoutPage from './pages/checkout/checkout.component';
@@ -20,6 +21,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,26 +1,27 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-import "./collection-preview.styles.scss";
+import CollectionItem from '../collection-item/collection-item.component';
 
-import CollectionItem from "../collection-item/collection-item.component";
+import {
+  CollectionPreviewContainer,
+  TitleContainer,
+  PreviewContainer
+} from './collection-preview.styles';
 
 const CollectionPreview = ({ title, items, history, match, routeName }) => (
-  <div className="collection-preview">
-    <h1
-      className="title"
-      onClick={() => history.push(`${match.path}/${routeName}`)}
-    >
+  <CollectionPreviewContainer>
+    <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>
       {title.toUpperCase()}
-    </h1>
-    <div className="preview">
+    </TitleContainer>
+    <PreviewContainer>
       {items
-        .filter((item, index) => index < 4)
+        .filter((item, idx) => idx < 4)
         .map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </PreviewContainer>
+  </CollectionPreviewContainer>
 );
 
 export default withRouter(CollectionPreview);
